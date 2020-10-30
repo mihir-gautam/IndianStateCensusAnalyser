1182129e6606583cee7237d0eba90f6dc7b51e1c
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-30 22:25:51 +0530

    [Mihir] Add. TC1.3 added to passing test case with incorrect filetype given

--- CensualAnalyserTest/UnitTest1.cs
+++ CensualAnalyserTest/UnitTest1.cs
@@ -53,5 +53,13 @@ namespace CensualAnalyserTest
             Assert.AreEqual(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND, censusException.eType);
             Assert.AreEqual(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND, stateException.eType);
         }
+        [Test]
+        public void GivenCorrectFilePathButIncorrectFileType_WhenReaded_ShouldReturnCustomException()
+        {
+            var censusException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(wrongIndianStateCensusFileType, Country.INDIA, indianStateCensusHeaders));
+            var stateException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(wrongIndianStateCodeFileType, Country.INDIA, indianStateCodeHeaders));
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE, censusException.eType);
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE, stateException.eType);
+        }
     }
 }
